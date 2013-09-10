0554166a21be7c6e6a9d00c61545f475e4de6199
David Persson <davidpersson@gmx.de>
2013-09-10 06:12:23 +0200

    Implementing is() method in generic classes.

--- src/Media/Process/Generic.php
+++ src/Media/Process/Generic.php
@@ -77,7 +77,18 @@ class Media_Process_Generic {
 	}
 
 	/**
-	 * Returns the lowercase name of the media type.
+	 * Checks if the name of the type (i.e. `'generic'` or `'image'`)
+	 * equals the provided one.
+	 *
+	 * @param string $name Name of the type to compare against.
+	 * @return boolean
+	 */
+	public function is($name) {
+		return $this->name() == $name;
+	}
+
+	/**
+	 * Returns the lowercase name of the type.
 	 *
 	 * @return string I.e. `'generic'` or `'image'`.
 	 */
