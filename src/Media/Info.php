a442b1853ea81ad0f7df9fc8efdca0b806d81840
David Persson <davidpersson@gmx.de>
2014-06-08 00:09:27 +0200

    Update docs with full namespacing.

--- src/Media/Info.php
+++ src/Media/Info.php
@@ -43,7 +43,7 @@ class Info {
 	 * @param array $config Valid values are:
 	 *                      - `'source'`: An absolute path to a file.
 	 *                      - `'adapters'`: Names or instances of media adapters (i.e. `['Gd']`).
-	 * @return \mm\Media\Info\Generic An instance of a subclass of `mm\Media\Process\Generic` or
+	 * @return \mm\Media\Info\Generic An instance of a subclass of `\mm\Media\Info\Generic` or
 	 *                               if type could not be mapped an instance of the that class
 	 *                               itself.
 	 */
