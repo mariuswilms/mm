7e2efbe5359bca830d6c743c82aba6af05c0e975
David Persson <davidpersson@gmx.de>
2015-10-28 14:28:28 +0100

    Add video info class (stub).

---
+++ src/Media/Info/Video.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * mm: the PHP media library
+ *
+ * Copyright (c) 2007-2014 David Persson
+ *
+ * Distributed under the terms of the MIT License.
+ * Redistributions of files must retain the above copyright notice.
+ */
+
+namespace mm\Media\Info;
+
+/**
+ * `Video` handles all video files.
+ */
+class Video extends \mm\Media\Info\Generic {}
+
+?>
